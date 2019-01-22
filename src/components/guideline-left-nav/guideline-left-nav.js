9dc4e38e0c335a89733e8efd02c4de2222fcdcf6
ianquigley-sap <ian.quigley@sap.com>
2019-01-22 12:36:12 +0000

    Update guideline-left-nav.js

--- src/components/guideline-left-nav/guideline-left-nav.js
+++ src/components/guideline-left-nav/guideline-left-nav.js
@@ -4,36 +4,31 @@ import './guideline-left-nav.scss'
 import { StaticQuery, graphql } from "gatsby"
 import { Location } from '@reach/router';
 
-const LeftNav = class extends React.Component {
-    render() {
-        return (
-            <nav className="guideline-left-nav">
-                <h1>Fiori For Web</h1>
-            <input></input>
-            <Location>
-            {({ location }) => {
-              return <Link className="main-nav" to={ (location.pathname.startsWith('/develop') ? 'develop' : 'design') + '/guidelines'}>Home</Link>
-            }}
-            </Location>
-            <h2>Foundation</h2>
-            <h2>Layouts &amp; Floorplans</h2>
-            <h2>Controls</h2>
-            {this.props.data.allMarkdownRemark.edges
-                .map(({ node: data }) => (
-                <Link className="control-menu" key={data.id} to={data.fields.slug}>{data.frontmatter.title}</Link>
-                ))  
-            }
-            <h2>Sample Apps</h2>
-            <h2>What's new</h2>
-            <h2>Resources</h2>
-            </nav>
-    )}
-};
+const LeftNav = ({ data }) => (
+  <nav className="guideline-left-nav">
+    <h1>Fiori For Web</h1>
+  <input></input>
+  <Location>
+  {({ location }) => {
+    return <Link className="main-nav" to={ (location.pathname.startsWith('/develop') ? 'develop' : 'design') + '/guidelines'}>Home</Link>
+  }}
+  </Location>
+  <h2>Foundation</h2>
+  <h2>Layouts &amp; Floorplans</h2>
+  <h2>Controls</h2>
+  {data.allMarkdownRemark.edges
+    .map(({ node: data }) => (
+      <Link className="control-menu" key={data.id} to={data.fields.slug}>{data.frontmatter.title}</Link>
+    ))  }
+  <h2>Sample Apps</h2>
+  <h2>What's new</h2>
+  <h2>Resources</h2>
+  </nav>
+)
 
 /**
  * Generates the guideline LHS navigation.
  */
-// const GuidelineLeftNav = class extends React.Component (
 export default props => (
 <StaticQuery
 query={graphql`{
@@ -58,5 +53,3 @@ query={graphql`{
     render={(data) => <LeftNav data={data} {...props} /> }
 />
 )
-
-// export default GuidelineLeftNav
