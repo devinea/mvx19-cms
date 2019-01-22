87233b70893c05de2fd90e619209654f4c6d5d6a
ianquigley-sap <ian.quigley@sap.com>
2019-01-22 14:11:19 +0000

    Remove extra import

--- src/components/guideline-left-nav/guideline-left-nav.js
+++ src/components/guideline-left-nav/guideline-left-nav.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import './guideline-left-nav.scss'
-import { StaticQuery, graphql } from "gatsby"
 import { Location } from '@reach/router';
 
 
