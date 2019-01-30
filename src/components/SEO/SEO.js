013d3b7140b90edc85f3e742592bce655448457f
Edouard Ouin <edouard.ouin@sap.com>
2019-01-30 19:18:33 +0000

    update the seo part

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+
 import favicon from '../../img/favicon.ico';
+import saplogo from '../../img/sap_logo.jpg';
 
 const detailsQuery = graphql`
   query DefaultSEOQuery {
@@ -25,20 +27,22 @@ const SEO = props => (
       return (
         <Helmet
           htmlAttributes={{
-            lang: 'en',
+            lang: 'en'
           }}
           title={title}
-          meta={[
-            { name: 'description', content: `${description}` }
-          ]}
+          meta={[{ name: 'description', content: `${description}` }]}
           titleTemplate={`%s - ${title}`}
           link={[
-            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+            {
+              rel: 'shortcut icon',
+              type: 'image/png',
+              href: `${favicon}`
+            }
           ]}
           meta={[
             { property: 'og:title', content: `${title}` },
             { property: 'og:type', content: 'website' },
-            { property: 'og:image', content: '/img/og-image.jpg' },
+            { property: 'og:image', content: `${saplogo}` },
             { property: 'og:description', content: `${description}` },
             { property: 'og:url', content: `${url}` }
           ]}
