3245d70e5b0e773dca2e52c6260a8d02a1d4db94
Edouard Ouin <edouard.ouin@sap.com>
2019-01-31 14:58:51 +0000

    fix markdown style

--- src/templates/blog-post/blog-post.js
+++ src/templates/blog-post/blog-post.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import { kebabCase } from 'lodash';
 import Helmet from 'react-helmet';
 import { graphql, Link } from 'gatsby';
+
 import Layout from '../../components/Layout';
+import { sharedStyles } from '../../components/theme';
+
 import Content, { HTMLContent } from '../../components/Content';
-import './blog-post.scss';
 
 export const BlogPostTemplate = ({
   content,
@@ -18,32 +20,33 @@ export const BlogPostTemplate = ({
   const PostContent = contentComponent || Content;
 
   return (
-    <section className='section blog'>
+    <article
+      css={{
+        padding: 20
+      }}
+    >
       {helmet || ''}
-      <div className='container content'>
-        <div className='columns'>
-          <div className='column is-10 is-offset-1'>
-            <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className='taglist'>
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+
+      <div css={[sharedStyles.markdown]}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+
+        <PostContent content={content} />
+
+        {tags && tags.length ? (
+          <div style={{ marginTop: `4rem` }}>
+            <h4>Tags</h4>
+            <ul className='taglist'>
+              {tags.map(tag => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
+        ) : null}
       </div>
-    </section>
+    </article>
   );
 };
 
@@ -84,9 +87,9 @@ BlogPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object
   }),
-	location: PropTypes.shape({
-		pathname: PropTypes.string.isRequired
-	})
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  })
 };
 
 export default BlogPost;
--- src/templates/design-guideline-post/design-guideline-post.js
+++ src/templates/design-guideline-post/design-guideline-post.js
@@ -7,8 +7,9 @@ import { graphql, Link } from 'gatsby';
 import Flex from '../../components/Flex';
 import Layout from '../../components/Layout';
 
+import { sharedStyles } from '../../components/theme';
+
 import Content, { HTMLContent } from '../../components/Content';
-import './design-guideline-post.scss';
 import GuidelineHeader from '../../components/guideline-header/guideline-header';
 import GuidelineLeftNav from '../../components/guideline-left-nav/guideline-left-nav';
 
@@ -23,32 +24,33 @@ export const DesignGuidelinePostTemplate = ({
   const PostContent = contentComponent || Content;
 
   return (
-    <section className='section guidelines-post'>
+    <article
+      css={{
+        paddingRight: 20
+      }}
+    >
       {helmet || ''}
-      <div className='container content'>
-        <div className='columns'>
-          <div className='column is-10 is-offset-1'>
-            <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className='taglist'>
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
+
+      <div css={[sharedStyles.markdown]}>
+        <h1>{title}</h1>
+        <p>{description}</p>
+
+        <PostContent content={content} />
+
+        {tags && tags.length ? (
+          <div style={{ marginTop: `4rem` }}>
+            <h4>Tags</h4>
+            <ul>
+              {tags.map(tag => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
+        ) : null}
       </div>
-    </section>
+    </article>
   );
 };
 
