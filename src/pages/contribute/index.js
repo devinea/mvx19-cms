30c131fb445d63e35e6ab57a29c2aa2eae616e3e
Ruarai Kirk <ruaraikirk@gmail.com>
2019-02-07 14:23:00 +0000

    Created contribute page

---
+++ src/pages/contribute/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import GuidelineHeader from '../../components/guideline-header/guideline-header'
+import './index.scss'
+import { graphql, StaticQuery } from "gatsby";
+import Img from 'gatsby-image';
+import Flex from "../../components/Flex";
+
+class ContributeIndexPage extends React.Component {
+  render() {
+    const { location } = this.props;
+    return (
+      <Layout location={location}>
+        <GuidelineHeader />
+        <Flex
+          direction='row'
+          shrink='0'
+          grow='1'
+          overflow='auto'
+          valign='stretch'
+          css={{
+            width: '100%',
+          }}
+        >
+        <div className="contribute-section">
+        <Img fixed={this.props.data.file.childImageSharp.fixed} />
+        </div>
+        </Flex>
+      </Layout>
+    )
+  }
+}
+
+
+export default props => (
+  <StaticQuery
+    query={graphql`query {
+      file(relativePath: { eq: "contributeImage.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fixed(width: 1200, height: 1500) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }`}
+    render={(data) => <ContributeIndexPage data={data} {...props} /> }
+  />
+)
