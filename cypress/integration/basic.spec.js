3704f6b9838dd9e40faa2ced3cfaacfee62e83b9
Eoin O'Neill <eoin.o-neill@sap.com>
2019-02-08 12:46:04 +0000

    test

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,11 @@
+describe('Basic Testing', function() {
+
+    it('Go to the Landing Page', function() {
+        cy.visit('http://localhost:9000')
+    })
+
+    it('Check the app is Fiori Plaform', function() {
+        cy.title().should('eq', 'SAP Fiori - SAP Fiori')
+    })
+    
+  })
