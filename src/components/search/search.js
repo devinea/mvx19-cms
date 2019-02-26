7fc193566bf769ac5566e7f9e3e0f1aa91c182d2
Ruarai Kirk <ruaraikirk@gmail.com>
2019-02-26 14:58:07 +0000

    Changed from using .substr to .substring

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -101,7 +101,7 @@ class Search extends Component {
               return (
                 <Link to={`/${page.path}`}>
                   <div className='search-result' key={uuid()}>
-                    <img className='thumbnail' src={page.featuredImage.substr(15)} />
+                    <img className='thumbnail' src={page.featuredImage.substring(15)} />
                     <div className='title'>{page.title}</div>
                     <div className='description'>{page.description}</div>
                     <div className='search-section'>{page.templateKeyName}</div>
