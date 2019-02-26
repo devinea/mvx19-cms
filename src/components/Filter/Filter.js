1e77092d86032a7522976c9a2790b2285a94b5e4
Austin Devine <austin.devine@sap.com>
2019-02-26 22:39:12 +0000

    Fix styling on version dropdown

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,7 +3,6 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import {StaticQuery, graphql } from 'gatsby';
 import { navigate } from 'gatsby';
-import { Location } from '@reach/router';
 const _ = require('lodash')
 
 
