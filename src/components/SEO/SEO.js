4e16ec21f567ed85b9f53ca2615cb8203100b81e
Merlin de la Haye <merlin.de.la.haye@sap.com>
2019-02-27 16:38:47 +0000

    new icon test

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
-import favicon from '../../img/favicon.ico';
 import saplogo from '../../img/sap_logo.jpg';
 
 const detailsQuery = graphql`
@@ -34,9 +33,30 @@ const SEO = props => (
           titleTemplate={`%s - ${title}`}
           link={[
             {
-              rel: 'shortcut icon',
-              type: 'image/png',
-              href: `${favicon}`
+              rel: "apple-touch-icon",
+              sizes: "180x180",
+              href: '/img/apple-touch-icon.png'
+            },
+            {
+              rel: 'icon',
+              type: 'image/vnd.microsoft.icon',
+              href: '/img/favicon.ico'
+            },
+            {
+              rel:"icon",
+              type:"image/png",
+              href:"/img/favicon-16x16.png",
+              sizes:"16x16"
+            },
+            {
+              rel:"icon",
+              type:"image/png",
+              href:"/img/favicon-32x32.png",
+              sizes:"32x32"
+            },
+            {
+              rel: "mask-icon",
+              href: `/img/safari-pinned-tab.svg`
             }
           ]}
           meta={[
