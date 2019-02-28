fd0053e892fd0fcab6231258e863e1b3010e6f9c
Merlin de la Haye <merlin.de.la.haye@sap.com>
2019-02-28 10:12:38 +0000

    remove invisible characters

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -29,24 +29,24 @@ const SEO = props => (
             lang: 'en'
           }}
           title={title}
-          meta={[{ name: 'description', content: `${description}` }]}
-          titleTemplate={`%s - ${title}`}
-          link={[
-          { href:​ "/img/icons/touch-icon-60x60.png", rel: ​"apple-touch-icon", sizes=​"60x60" },
-          { href:​ "/img/icons/touch-icon-120x120.png", rel: ​"apple-touch-icon", sizes=​"120x120" },
-          { href:​ "/img/icons/touch-icon-180x180.png", rel: ​"apple-touch-icon", sizes=​"180x180" },
-          { href:​ "/img/icons/touch-icon-1024x1024.png", rel: ​"apple-touch-icon", sizes=​"1024x1024" },
-          { href: '/img/icons/favicon.ico', rel: 'icon', type: 'image/vnd.microsoft.icon'},
-          { rel: 'shortcut icon', type: 'image/vnd.microsoft.icon', href: '/img/icons/favicon.ico' },
-          { rel: "mask-icon", href: `/img/icons/safari-pinned-tab.svg` }
-            ]}
           meta={[
+            { name: 'description', content: `${description}` },
             { property: 'og:title', content: `${title}` },
             { property: 'og:type', content: 'website' },
             { property: 'og:image', content: `${saplogo}` },
             { property: 'og:description', content: `${description}` },
             { property: 'og:url', content: `${url}` }
-          ]}
+            ]}
+          titleTemplate={`%s - ${title}`}
+          link={[
+            { href: "/img/icons/touch-icon-60x60.png", rel: "apple-touch-icon", sizes: "60x60" },
+            { href: "/img/icons/touch-icon-120x120.png", rel: "apple-touch-icon", sizes: "120x120" },
+            { href: "/img/icons/touch-icon-180x180.png", rel: "apple-touch-icon", sizes: "180x180" },
+            { href: "/img/icons/touch-icon-1024x1024.png", rel: "apple-touch-icon", sizes: "1024x1024" },
+            { href: '/img/icons/favicon.ico', rel: 'icon', type: 'image/vnd.microsoft.icon'},
+            { rel: 'shortcut icon', type: 'image/vnd.microsoft.icon', href: '/img/icons/favicon.ico' },
+            { rel: "mask-icon", href: `/img/icons/safari-pinned-tab.svg` }
+            ]}
         />
       );
     }}
