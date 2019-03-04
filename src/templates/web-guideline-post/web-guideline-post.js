81ba2a0cf88da69b6aa3b579f77203338774eb3f
Austin Devine <austin.devine@sap.com>
2019-03-04 13:48:33 +0000

    add navTitle boolean

--- src/templates/web-guideline-post/web-guideline-post.js
+++ src/templates/web-guideline-post/web-guideline-post.js
@@ -164,6 +164,7 @@ export const pageQuery = graphql`
                           title
                           parentId
                           hasChildren
+                          navTitle
                       }
                   }
 
