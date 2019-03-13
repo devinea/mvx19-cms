42e7e75e1b05598960fa8c49b4e6b078fab8b2a2
Christos Koutsiaris <christos.koutsiaris@sap.com>
2019-03-13 17:02:14 +0000

    css changes

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,39 +1,33 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import { Link } from 'gatsby';
-import { colors } from '../theme';
 import selectArrowIcon from './../../img/select-arrow.svg';
-
-import { css } from '@emotion/core';
 
 const Dropdown = (props) => {
     return (
-    <div css={css`
-    background: url(${selectArrowIcon}) no-repeat 100px 10px;
-    height: 30px;
-    overflow: hidden;
-    width: 130px;
-    margin: 30px 10px;
-    `}>
-        <select css={css`
-            background: transparent;
-            border: none;
-            font-size: 16px;
-            height: 30px;
-            width: 150px;
-            outline: none;
-            font-weight: 600;
-        `}>
-            {props.options.map((o, idx) => {
-                return (<option css={css`
-
-                `}
-                key={idx}
-                value={o.label}>{o.label}</option>);
-            }
-            )}
-        </select>
-    </div>
-)};
+        <div css={{
+            background: `url(${selectArrowIcon}) no-repeat 100px 10px`,
+            height: 30,
+            overflow: 'hidden',
+            width: 130,
+            margin: '30px 10px'
+        }}>
+            <select css={{
+                background: 'transparent',
+                border: 'none',
+                fontSize: 16,
+                height: 30,
+                width: 150,
+                outline: 'none',
+                fontWeight: 600
+            }}>
+                {props.options
+                    .map((o, idx) => {
+                        return (<option key={idx} value={o.node.title}>{o.node.title}</option>);
+                    })
+                }
+            </select>
+        </div>
+    )
+};
 
 export default Dropdown;
