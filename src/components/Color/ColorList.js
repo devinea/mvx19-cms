077a794ade0d4f6d6ba81eadf9f2487511b08244
Ruaraí Kirk <41237990+ruaraikirk@users.noreply.github.com>
2019-03-14 11:21:16 +0000

    Feature/197/create preview for color collection (#158)
    * Initial commit of CMS preview for colors collection.
    
    * Updates to cms.js to allow preview of colors, work ongoing.
    
    * Using components to pass color data.
    
    * Achieved access to color data from nested widgets and started styling.
    
    * Color component and preview ready for review.
    
    * Added minor updates.
    
    * Addressed change requests as per PR review  and some minor refactoring.

---
+++ src/components/Color/ColorList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Color from './Color'
+
+class ColorList extends React.Component {
+  render() {
+    const { name, colors } = this.props;
+    return (
+    <section>
+    <h2>Color Group: {name}</h2>
+    <div>
+        { colors.map((color, index) => 
+        <Color
+            key={index}
+            name={color.name}
+            light={color.light}
+            dark={color.dark}
+            description={color.description}
+        />
+        )}
+    </div>
+    </section>
+    )
+  }
+}
+
+ColorList.defaultProps = {
+  name: "<Color Group Name>",
+  colors: [{}],
+}
+
+ColorList.propTypes = {
+  name: PropTypes.string,
+  colors: PropTypes.object,
+}
+
+export default ColorList
