f50aea3fdeb02b8ab8cd932b4fdea07113cdbb2b
Edouard Ouin <edouard.ouin@sap.com>
2019-03-20 11:15:32 +0000

    add the new hero banner on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,141 +1,178 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
-import Layout from '../components/Layout';
+
+import { graphql, StaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 
-import Search from '../components/search/search';
+import Layout from '../components/Layout';
+import Flex from '../components/Flex';
 
-import './index.scss';
+import { colors, media } from '../components/theme';
 
-export default class IndexPage extends React.Component {
+class IndexPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      searchToggle: 'off'
-    };
-    this.handler = this.handler.bind(this);
-  }
-
-  handler(toggleVal) {
-    this.setState({
-      searchToggle: toggleVal
-    });
   }
 
   render() {
-    const { data, location } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
+    const { location } = this.props;
 
     return (
       <Layout location={location}>
-        <section className={`${this.state.searchToggle === 'on' ? 'search-results' : 'trending'}`}>
-          <div className="content">
-            <div className="header">Explore, Design and Develop with the Design System for the Enterprise</div>
-            <div className="search"><Search name="search" searchToggle={this.handler} /></div>
-
-            <div className="trending-suggestions">
-              <div className="trending-title">Trending</div>
-              {['Fiori Toolkit v1.1',
-                'Fiori Fundamentals',
-                'Sample Apps',
-                'Getting Started with Fiori',
-                '72 TypeFace'].map((o, idx) => {
-                  return <div key={idx}>{o}</div>
-                })}
-            </div>
-          </div>
-          <div className="trending-footer"></div>
-        </section>
-        <section className="section whatsnew">
-          <div className="whatsnew-wrapper">
-            <div className="container">
-              <div className="content">
-                <div className="whatsnew-header">
-                  <h1 className="whatsnew-title">what's new</h1>
-                  <div className="whatsnew-options">
-                    <div className="selected">web</div>
-                    <div>Mobile</div>
-                    <div>CUX</div>
-                    <div>AR/VR</div>
-                  </div>
-                </div>
+        <div
+          css={{
+            width: '100%',
+            overflow: 'hidden',
+            position: 'relative',
+            backgroundSize: '3000px 600px',
+            backgroundRepeat: 'no-repeat',
+            backgroundPositionX: 'center',
+            backgroundImage:
+              'radial-gradient(ellipse at 50% -27%,white 49.9%,#354A5F 50%)'
+          }}
+        >
+          <Flex
+            direction='column'
+            css={{
+              margin: '0 auto',
+              [media.greaterThan('small')]: {
+                minWidth: media.getSize('small').width,
+                maxWidth: media.getSize('small').width
+              },
+              [media.greaterThan('medium')]: {
+                minWidth: media.getSize('medium').width,
+                maxWidth: media.getSize('medium').width
+              },
+              [media.greaterThan('large')]: {
+                minWidth: media.getSize('large').width,
+                maxWidth: media.getSize('large').width
+              },
+              [media.greaterThan('xlarge')]: {
+                minWidth: media.getSize('xlarge').width,
+                maxWidth: media.getSize('xlarge').width
+              }
+            }}
+          >
+            <section
+              css={{
+                width: '100%',
+                [media.greaterThan('small')]: {
+                  marginTop: 82,
+                  marginBottom: 172,
+                  padding: 0
+                },
+                [media.greaterThan('medium')]: {
+                  marginTop: 82,
+                  marginBottom: 172,
+                  padding: 0
+                },
+                [media.greaterThan('large')]: {
+                  marginTop: 82,
+                  marginBottom: 172,
+                  padding: 0
+                },
+                [media.greaterThan('xlarge')]: {
+                  marginTop: 82,
+                  marginBottom: 172,
+                  padding: 0
+                }
+              }}
+            >
+              <h1
+                css={{
+                  margin: 0,
+                  textAlign: 'left',
+                  width: 480,
+                  lineHeight: '50px',
+                  fontWeight: 300,
+                  color: colors.gray_700,
+                  [media.greaterThan('small')]: {
+                    fontSize: 28
+                  },
+                  [media.greaterThan('medium')]: {
+                    fontSize: 40
+                  },
+                  [media.greaterThan('large')]: {
+                    fontSize: 40
+                  },
+                  [media.greaterThan('xlarge')]: {
+                    fontSize: 40
+                  }
+                }}
+              >
+                Explore, Design and Develop with the Design System for the
+                Enterprise
+              </h1>
+            </section>
+            <section
+              css={{
+                margin: '0 auto',
+                position: 'absolute',
+                [media.greaterThan('small')]: {
+                  minWidth: media.getSize('small').width,
+                  maxWidth: media.getSize('small').width
+                },
+                [media.greaterThan('medium')]: {
+                  minWidth: media.getSize('medium').width,
+                  maxWidth: media.getSize('medium').width
+                },
+                [media.greaterThan('large')]: {
+                  minWidth: media.getSize('large').width,
+                  maxWidth: media.getSize('large').width
+                },
+                [media.greaterThan('xlarge')]: {
+                  minWidth: media.getSize('xlarge').width,
+                  maxWidth: media.getSize('xlarge').width
+                }
+              }}
+            >
+              <div
+                css={{
+                  position: 'absolute',
+                  display: 'flex',
+                  left: '60%',
+                  top: 50,
+                  height: 354
+                }}
+              >
+                <Img
+                  fixed={this.props.data.file.childImageSharp.fixed}
+                  style={{
+                    height: '354px'
+                  }}
+                  imgStyle={{
+                    objectPosition: 'top left'
+                  }}
+                  placeholderStyle={{
+                    transition: 'none'
+                  }}
+                />
               </div>
-              <div className="new-lhs">
-                <div className="new-lhs-title">Learn about the latest updates and announcements to the Fiori Design System.</div>
-                <div className="new-lhs-option selected">all</div>
-                <div className="new-lhs-option">designer</div>
-                <div className="new-lhs-option">developer</div>
-              </div>
-              <div className="new-rhs">
-                {posts
-                  .map(({ node: post }) => (
-                    <Link to={post.fields.slug} key={post.id}>
-                      <div
-                        className="content whatsnew-items"
-                        id={post.img}
-                        key={post.id}
-                      >
-                        <div className="newitem-item-content">
-                          <Img className="newitem-item-thumbnail" sizes={post.frontmatter.picture.childImageSharp.sizes} />
-                          <p>
-                            <span className="newitem-title">{post.frontmatter.title}</span>
-                          </p>
-                          <div className="newitem-details">
-                            {post.frontmatter.description}
-                          </div>
-                          <span className="newitem-more">Read More</span>
-                          <div className="newitem-date">{post.frontmatter.date}</div>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-              </div>
-            </div>
-          </div>
-        </section>
+            </section>
+          </Flex>
+        </div>
       </Layout>
-    )
+    );
   }
 }
 
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array
-    })
-  })
-};
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-
-          frontmatter {
-            title
-            templateKey
-            description
-            date(formatString: "MMMM DD, YYYY")
-            picture {
-              childImageSharp {
-                sizes(maxWidth: 75) {
-                  ...GatsbyImageSharpSizes
-                }
-              }
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        file(relativePath: { eq: "ipadmockup.png" }) {
+          childImageSharp {
+            # Specify the image processing specifications right in the query.
+            # Makes it trivial to update as your page's design changes.
+            # fixed(width: 1742, height: 1052) {
+            #   ...GatsbyImageSharpFixed
+            # }
+            fixed(height: 504) {
+              ...GatsbyImageSharpFixed
             }
           }
         }
       }
-    }
-  }
-`;
+    `}
+    render={data => <IndexPage data={data} {...props} />}
+  />
+);
